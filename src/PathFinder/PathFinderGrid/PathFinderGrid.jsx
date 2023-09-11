f79fdfef6cc1178c636226dd402d8861bbd8dc6b
Anouar <anouarabouerraja@gmail.com>
2023-09-11 15:38:13 +0100

    fixes some bugs

--- src/PathFinder/PathFinderGrid/PathFinderGrid.jsx
+++ src/PathFinder/PathFinderGrid/PathFinderGrid.jsx
@@ -7,28 +7,28 @@ import {
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import { lightGreen, deepOrange } from "@mui/material/colors";
 import PersonPinIcon from "@mui/icons-material/PersonPin";
+import _ from "lodash";
 
 function PathFinderGrid() {
   const [nodes, setNodes] = useState([]);
-  const [startNode, setStart] = useState([7, 4]);
-  const [finishNode, setFinsih] = useState([7, 33]);
-  const [isMouseDown, setIsMouseDown] = useState([false, ""]);
+  const [startNode, setStartNode] = useState({ row: 10, col: 5 });
+  const [finishNode, setFinsih] = useState({ row: 10, col: 45 });
+  const [mouseState, setMouseState] = useState({
+    isMouseDown: false,
+    nodeType: "",
+  });
   const [isRestartDisabled, setIsRestartDisabled] = useState(true);
-  const [isWallButnDisabled, setIsWallButnDisabled] = useState(false);
   const [isVisButnDisabled, setIsVisButnDisabled] = useState(false);
-  const [isAddWall, setIsAddWall] = useState(false);
 
   useEffect(() => {
     const initialNodes = [];
 
-    for (let row = 0; row <= 15; row++) {
+    for (let row = 0; row <= 20; row++) {
       const rowNodes = [];
-      for (let col = 0; col <= 50; col++) {
+      for (let col = 0; col <= 52; col++) {
         const currentNode = {
           col,
           row,
-          startNode: row === startNode[0] && col === startNode[1],
-          finishNode: row === finishNode[0] && col === finishNode[1],
           distance: Infinity,
           previousNode: null,
           wall: false,
@@ -39,50 +39,55 @@ function PathFinderGrid() {
     }
 
     setNodes(initialNodes);
-  }, [startNode, finishNode, isRestartDisabled]);
+  }, [isRestartDisabled]);
 
   // Gets the node type on mouse down
-  const activateMouseState = (node) => {
-    const nodeType = node.startNode
+  const activateMouseState = async (node) => {
+    const { row, col } = node;
+    const nodeType = _.isEqual({ row, col }, startNode)
       ? "start"
-      : node.finishNode
+      : _.isEqual({ row, col }, finishNode)
       ? "finish"
       : "wall";
-    setIsMouseDown([true, nodeType]);
+
+    setMouseState({ isMouseDown: true, nodeType: nodeType });
+    console.log(mouseState);
   };
   // Updates the node's row and column on mouse enter
   const getCoordinates = async (node) => {
-    if (isMouseDown[0]) {
+    const { row, col } = node;
+    if (mouseState.isMouseDown && !isVisButnDisabled) {
       if (
-        isMouseDown[1] === "wall" &&
-        isAddWall &&
-        !node.startNode &&
-        !node.finishNode
+        mouseState.nodeType === "wall" &&
+        !_.isEqual({ row, col }, startNode) &&
+        !_.isEqual({ row, col }, finishNode)
       ) {
         document.getElementById(`node-${node.row}-${node.col}`).className =
           "node node-wall";
-        await updateNodeProperty(node.row, node.col, "wall", true);
+        updateNodeProperty(node.row, node.col, "wall", true);
       } else if (
-        isMouseDown[1] === "start" &&
+        mouseState.nodeType === "start" &&
         !node.wall &&
-        JSON.stringify([node.row, node.col]) !== JSON.stringify(finishNode)
+        !_.isEqual({ row, col }, finishNode)
       ) {
-        setStart([node.row, node.col]);
+        setStartNode({ row: node.row, col: node.col });
       } else if (
-        isMouseDown[1] === "finish" &&
+        mouseState.nodeType === "finish" &&
         !node.wall &&
-        JSON.stringify([node.row, node.col]) !== JSON.stringify(startNode)
-      )
-        setFinsih([node.row, node.col]);
+        !_.isEqual({ row, col }, startNode)
+      ) {
+        setFinsih({ row: node.row, col: node.col });
+      }
     }
   };
   // Stops getCoordinates() when mouse is up
-  const deactivateMouseState = (node) => {
-    setIsMouseDown([false, ""]);
+  const deactivateMouseState = () => {
+    setMouseState({ ...mouseState, isMouseDown: false, nodeType: "" });
   };
 
   // Updates a given prop of a given node
   const updateNodeProperty = async (row, col, propName, propValue) => {
+    console.log(row);
     setNodes((prevNodes) => {
       return prevNodes.map((nodeRow, rowIndex) => {
         return nodeRow.map((node, colIndex) => {
@@ -97,18 +102,16 @@ function PathFinderGrid() {
   };
 
   const startAlgorithm = async () => {
-    setIsAddWall(false);
-    setIsWallButnDisabled(true);
     setIsVisButnDisabled(true);
-    setIsMouseDown([false, ""]);
+    setMouseState({ ...mouseState, isMouseDown: false, nodeType: "" });
     updateIconsAnimation("icons-pointer-event");
     const visitedNodesInOrder = dijkstra(
       nodes,
-      nodes[startNode[0]][startNode[1]],
-      nodes[finishNode[0]][finishNode[1]]
+      nodes[startNode.row][startNode.col],
+      nodes[finishNode.row][finishNode.col]
     );
     const nodesInShortestPathOrder = getNodesInShortestPathOrder(
-      nodes[finishNode[0]][finishNode[1]]
+      nodes[finishNode.row][finishNode.col]
     );
     animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
   };
@@ -145,8 +148,6 @@ function PathFinderGrid() {
 
   const restartAlgorithm = () => {
     clearAnimation();
-    setIsWallButnDisabled(false);
-    setIsAddWall(false);
     setIsVisButnDisabled(false);
     updateIconsAnimation("icons-animation");
   };
@@ -163,76 +164,68 @@ function PathFinderGrid() {
   // Update start/finish icons' css
   const updateIconsAnimation = (className) => {
     document
-      .getElementById(`node-${startNode[0]}-${startNode[1]}`)
+      .getElementById(`node-${startNode.row}-${startNode.col}`)
       .querySelector("div").className = `icon ${className}`;
     document
-      .getElementById(`node-${finishNode[0]}-${finishNode[1]}`)
+      .getElementById(`node-${finishNode.row}-${finishNode.col}`)
       .querySelector("div").className = `icon ${className}`;
-  };
-
-  const toggleIsAddWall = () => {
-    updateIconsAnimation("icons-pointer-event");
-    setIsAddWall((prevIsAddWall) => !prevIsAddWall);
   };
 
   return (
-    <div class="lines">
+    <div class="container">
       <div class="line"></div>
       <div class="line"></div>
       <div class="line"></div>
       <div className="grid">
-        <button
-          class="button"
-          onClick={startAlgorithm}
-          disabled={isVisButnDisabled}
-        >
-          Visualize
-        </button>
-        <button
-          class="button"
-          onClick={restartAlgorithm}
-          disabled={isRestartDisabled}
-        >
-          Restart
-        </button>
-        <button
-          class="button"
-          onClick={toggleIsAddWall}
-          disabled={isWallButnDisabled}
-        >
-          {!isAddWall && "Add Walls"}
-          {isAddWall && "Stop Adding Walls"}
-        </button>
-        {nodes.map((row, rowIndex) => (
-          <div className="row">
-            {row.map((node, nodeIndex) => (
-              <div
-                id={`node-${node.row}-${node.col}`}
-                className="node"
-                onMouseDown={() => activateMouseState(node)}
-                onMouseUp={() => deactivateMouseState(false)}
-                onMouseEnter={() => getCoordinates(node)}
-              >
-                {node.startNode && (
-                  <div className="icon icons-animation">
-                    <PersonPinIcon
-                      sx={{ color: lightGreen[700] }}
-                      fontSize="medium"
-                    />
-                  </div>
-                )}
-                {node.finishNode && (
-                  <div className="icon icons-animation">
-                    <LocationOnIcon
-                      sx={{ color: deepOrange[500] }}
-                      fontSize="medium"
-                    />
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
-        ))}
+        <div className="buttons">
+          <button
+            class="button"
+            onClick={startAlgorithm}
+            disabled={isVisButnDisabled}
+          >
+            Visualize
+          </button>
+          <button
+            class="button"
+            onClick={restartAlgorithm}
+            disabled={isRestartDisabled}
+          >
+            Restart
+          </button>
+        </div>
+        <div>
+          {nodes.map((row, rowIndex) => (
+            <div className="row">
+              {row.map((node, nodeIndex) => (
+                <div
+                  id={`node-${node.row}-${node.col}`}
+                  className="node"
+                  onMouseDown={() => activateMouseState(node)}
+                  onMouseUp={() => deactivateMouseState()}
+                  onMouseEnter={() => getCoordinates(node)}
+                >
+                  {node.row === startNode.row && node.col === startNode.col && (
+                    <div className="icon icons-animation">
+                      <PersonPinIcon
+                        sx={{ color: lightGreen[700] }}
+                        fontSize="medium"
+                      />
+                    </div>
+                  )}
+                  {node.row === finishNode.row &&
+                    node.col === finishNode.col && (
+                      <div className="icon icons-animation">
+                        <LocationOnIcon
+                          sx={{ color: deepOrange[500] }}
+                          fontSize="medium"
+                        />
+                      </div>
+                    )}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
