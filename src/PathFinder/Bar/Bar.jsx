0e1bad3675cb29bd195a3e8d4ecb7003483c7899
Anouar <anouarabouerraja@gmail.com>
2023-09-14 19:02:56 +0100

    adds info alert

--- src/PathFinder/Bar/Bar.jsx
+++ src/PathFinder/Bar/Bar.jsx
@@ -1,14 +1,25 @@
 import { Slider } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import Collapse from "@mui/material/Collapse";
 import "./Bar.css";
+import { useState } from "react";
 
-const theme = createTheme({
+const btnTheme = createTheme({
   palette: {
     primary: { main: "#63617a" },
   },
 });
 
-const speed = { '1': 110, '2': 60, '3': 10 };
+const alertTheme = createTheme({
+  palette: {
+    info: { main: "#63617a" },
+  },
+});
+
+const speed = { 1: 110, 2: 60, 3: 10 };
 
 function Bar(props) {
   const {
@@ -20,6 +31,8 @@ function Bar(props) {
     setAlgorithmSpeed,
   } = props;
 
+  const [open, setOpen] = useState(true);
+
   const handleAlgorithmSpeed = (e, value) => {
     if (speed.hasOwnProperty(value)) {
       setAlgorithmSpeed(speed[value]);
@@ -28,6 +41,49 @@ function Bar(props) {
   return (
     <div>
       <div className="bar">
+        <Box
+          style={{
+            width: "45%",
+            position: "fixed",
+            "margin-left": 10,
+            top: 100,
+            opacity: 0.9,
+            whiteSpace: "pre-line",
+            textAlign: "left",
+          }}
+        >
+          <Collapse in={open}>
+            <ThemeProvider theme={alertTheme}>
+              <Alert
+                severity="info"
+                icon={false}
+                action={
+                  <HighlightOffIcon
+                    aria-label="close"
+                    fontSize="small"
+                    color="inherit"
+                    onClick={() => {
+                      setOpen(false);
+                    }}
+                  ></HighlightOffIcon>
+                }
+                sx={{ mb: 2 }}
+              >
+                Drag and drop the start and finish icons to different cells.
+                {"\n"}
+                Place obstacles on the grid by clicking on the cells.{"\n"}
+                Choose one of the path-finding algorithms from the dropdown
+                menu.{"\n"}
+                Use the slider to adjust the speed of the algorithm
+                visualization.{"\n"}
+                Click the "Visualize" button to start the algorithm.{"\n"}
+                To start a new visualization, click the "Restart" button and
+                repeat the process.
+              </Alert>
+            </ThemeProvider>
+          </Collapse>
+        </Box>
+
         <div className="select">
           <select
             className="select-menu"
@@ -42,7 +98,7 @@ function Bar(props) {
           </select>
           <div className="slider">
             Speed:
-            <ThemeProvider theme={theme}>
+            <ThemeProvider theme={btnTheme}>
               <Slider
                 aria-label="Speed"
                 // getAriaValueText={valuetext}
