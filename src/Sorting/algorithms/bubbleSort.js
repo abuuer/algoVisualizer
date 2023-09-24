62bf6a4497b68d89475b31b90fb617ecc423875e
Anouar <anouarabouerraja@gmail.com>
2023-09-24 01:04:16 +0100

    update BS

--- src/Sorting/algorithms/bubbleSort.js
+++ src/Sorting/algorithms/bubbleSort.js
@@ -1,10 +1,12 @@
+import { swapNCompareWithDelay } from "./helpers";
+
 export default async function bubbleSort(recs, setRectangles) {
   for (let i = 0; i < recs.length; i++) {
     let swapped = false;
     for (let j = 0; j < recs.length - i - 1; j++) {
       document.getElementById(`${j}`).className = "rectangle rectangle-current";
 
-      await swapWithDelay(recs, j, j + 1);
+      await swapNCompareWithDelay(recs, j, j + 1);
       setRectangles([...recs]);
       swapped = true;
       document.getElementById(`${j}`).className = "rectangle";
@@ -13,17 +15,3 @@ export default async function bubbleSort(recs, setRectangles) {
     if (!swapped) break;
   }
 }
-
-const swapWithDelay = async (recs, i, j) => {
-  const delay = 5000 / recs.length;
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      if (recs[i] > recs[j]) {
-        let temp = recs[i];
-        recs[i] = recs[j];
-        recs[j] = temp;
-      }
-      resolve();
-    }, delay);
-  });
-};
