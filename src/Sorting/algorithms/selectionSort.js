d3197032ab5037687b893922fa01ac641804c0cd
Anouar <anouarabouerraja@gmail.com>
2023-10-09 15:28:32 +0100

    updates ss visuals

--- src/Sorting/algorithms/selectionSort.js
+++ src/Sorting/algorithms/selectionSort.js
@@ -10,12 +10,19 @@ export default async function selectionSort(recs, setRectangles) {
       document.getElementById(`${j}`).className = "rectangle rectangle-red";
       await sleep(recs.length);
       if (recs[j] < recs[min_idx]) {
+        document.getElementById(`${min_idx}`).className = "rectangle";
+        document.getElementById(`${i}`).className =
+          "rectangle rectangle-current";
         min_idx = j;
+        document.getElementById(`${min_idx}`).className =
+          "rectangle rectangle-pivot";
+        continue;
       }
       document.getElementById(`${j}`).className = "rectangle";
     }
     await sleep(recs.length);
     await swap(recs, min_idx, i);
+    document.getElementById(`${min_idx}`).className = "rectangle";
     setRectangles([...recs]);
     document.getElementById(`${i}`).className = "rectangle";
   }
