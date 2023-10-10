a2d811a058e5103431aaf9e9899d5491ce3ca20c
Anouar <anouarabouerraja@gmail.com>
2023-10-10 19:29:53 +0100

    adds gh and lkdn links

--- src/AlgoVisualizer/Home/Home.jsx
+++ src/AlgoVisualizer/Home/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.scss";
 import { Link } from "react-router-dom";
-
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
 function Home() {
   return (
     <div className="home">
@@ -8,7 +9,7 @@ function Home() {
         <img src={require("../Common/logo.png")} alt="Logo" className="logo" />
       </header>
       <div className="container">
-        <h4 class="wordCarousel">
+        <h1 class="wordCarousel">
           <span>WELCOME TO </span>
           <div>
             <ul class="flip3">
@@ -17,7 +18,7 @@ function Home() {
               <li>SORTVISUALIZER</li>
             </ul>
           </div>
-        </h4>
+        </h1>
         <p>
           Discover the magic of algorithms through mesmerizing visualizations
           and gain valuable insights into their inner workings with
@@ -31,6 +32,17 @@ function Home() {
             PathVisualizer
           </Link>
         </div>
+      </div>
+      <div class="footer">
+        <a href="https://github.com/abuuer/algoVisualizer" target="_target">
+          <GitHubIcon sx={{ color: "#555" }} fontSize="medium" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/anouar-abouerraja/"
+          target="_target"
+        >
+          <LinkedInIcon sx={{ color: "#555" }} fontSize="medium" />
+        </a>
       </div>
     </div>
   );
